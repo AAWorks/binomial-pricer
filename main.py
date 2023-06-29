1047af8779033e4d310e66693360847a28bc0840
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-06-29 11:55:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":graph:")
+st.title('Options Pricing: From Binomial to Black Shouls :graph:')
+st.caption("By Alejandro Alonso, Roman Chenoweth, Maggie Schwierking")
