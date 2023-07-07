427c2098f9945815e8e7059b638a47305264cf59
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-07 09:40:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":graph:")
-st.title('Options Pricing: From Binomial to Black Shouls :graph:')
-st.caption("By Alejandro Alonso, Roman Chenoweth, Maggie Schwierking")
+st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":chart_with_upwards_trend:")
+st.title('Options Pricing: From Binomial to Black Shouls :chart_with_upwards_trend:')
+st.caption("By Alejandro Alonso and Roman Chenoweth")
+
+
