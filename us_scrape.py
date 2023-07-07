0726b79807d20e6e05663f33e9c898f0d81decc9
AAWorks <axalonso12@gmail.com>
2023-07-07 16:00:29 +0000

    us options scraper barebones

---
+++ us_scrape.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class USOptions:
+    _headers: dict
+
+    def __init__(self):
+        with open('keys/polygon.txt', 'r') as keyfile:
+            key = keyfile.readline().strip()
+
+        self._headers = {
+            'Authorization': f'Bearer {key}'
+        }
+
+        self._base_url = 'https://api.polygon.io/'
+    
+    @property
+    def headers(self):
+        return self._headers
+
+    def get_req_url(self, extension = ""):
+        return self._base_url + extension
