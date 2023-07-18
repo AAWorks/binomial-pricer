4650dbad2481328d3bae895fff8321a0b74bbb01
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-18 19:58:33 -0400

    ui update

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import streamlit as st
 
 st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":chart_with_upwards_trend:")
-st.title('Options Pricing: From Binomial to Black Shouls :chart_with_upwards_trend:')
+st.title('Options Pricing: Binomial and Black Sholes with Monte Carlo Sampling')
 st.caption("By Alejandro Alonso and Roman Chenoweth")
 
 
-region = st.selectbox("Select Market Region", ("United States of America", "European Union"))
+binomial, blk = st.tabs(["Binomial Model", "Black Sholes"])
 
-if region == "United States of America":
-    with st.form("us"):
-        st.write("hi")
-
-elif region == "European Union":
-    with st.form("eu"):
-        st.write("hi")
-
-else:
-    st.warning("No Market Region Selected")
+with binomial:
+    st.info("Binomial Options Pricing")
+with blk:
+    st.info("Black Sholes Model for European Options Pricing")
 
