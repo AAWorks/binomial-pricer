3a63f4858ce2def78e7f8083c2eca2c6263a47b1
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-21 01:21:02 -0400

    ui kinda

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from models.black_scholes import BlackScholesOption
 
 from polygon import Polygon
 
-st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":chart_with_upwards_trend:")
-st.title('Options Pricing: Binomial and Black Scholes with Monte Carlo Sampling')
-st.caption("By Alejandro Alonso and Roman Chenoweth")
+st.set_page_config(layout="wide", page_title="Options Pricing", page_icon=":gear:")
+st.title('Quantitative Options Pricing') 
+st.caption('Via Black Scholes, Binomial Trees, and Monte Carlo Sampling')
+st.caption("Alejandro Alonso and Roman Chenoweth")
 
 
 binomial, blk = st.tabs(["Binomial Model", "Black Scholes"])
