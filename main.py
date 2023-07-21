8c8d9efdea69c4adb5aabd2f7c08ec257a201c05
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-21 01:13:23 -0400

    imports

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import streamlit as st
 
+from models.binomial_tree import BinomialTreeOption
+from models.black_scholes import BlackScholesOption
+#from models.monte_carlo import MonteCarloOption
+
+from polygon import Polygon
+
 st.set_page_config(layout="wide", page_title="SOP Bot", page_icon=":chart_with_upwards_trend:")
-st.title('Options Pricing: Binomial and Black Sholes with Monte Carlo Sampling')
+st.title('Options Pricing: Binomial and Black Scholes with Monte Carlo Sampling')
 st.caption("By Alejandro Alonso and Roman Chenoweth")
 
 
-binomial, blk = st.tabs(["Binomial Model", "Black Sholes"])
+binomial, blk = st.tabs(["Binomial Model", "Black Scholes"])
 
 with binomial:
     st.info("Binomial Options Pricing")
 with blk:
-    st.info("Black Sholes Model for European Options Pricing")
+    st.info("Black Scholes Model for European Options Pricing")
 
