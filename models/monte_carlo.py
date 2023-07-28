bd627232e3ff189239de2d12de6d0eda49838072
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-28 00:27:34 -0400

    standard option pricing w monte carlo sims

--- models/monte_carlo.py
+++ models/monte_carlo.py
@@ -1,19 +1,42 @@
-import numpy as np
-
+import torch
 
 class MonteCarloOption:
-    def __init__(self):
-        pass
+    def __init__(self, strike, stock, vol, time, risk_rate, dividend_rate):
+        self._vol = vol
+        self._time = time
+        self._r_rate = risk_rate
+        self._d_rate = dividend_rate
+        self._stock = stock
+        self._strike = strike
 
     @property
     def price(self):
-        pass
+        torch.manual_seed(42)
+        scenarios = 1000000
+        dW = self._vol * self._time ** 0.5 * torch.randn(size=(scenarios,))
+        r = torch.exp((self._r_rate - self._d_rate - self._vol * self._vol / 2) * self._time + dW)
+
+        payoff = torch.max(self._strike - self._stock*r, torch.zeros(size=(scenarios,)))
+        return torch.mean(payoff) * torch.exp(-self._rate*self._time)
+    
+    @property
+    def greeks(self):
+        ov = self.price
+        ov.backward()
+        return {
+            "delta": self._stock.grad,
+            "rho": self._rate.grad,
+            "vega": self._vol.grad,
+            "theta": self._time.grad,
+            "epsilon": self._dividend.grad,
+            "strike_greek": self._strike.grad
+        }
 
     def viz(self):
         pass
 
 
-class GymSim:
+class GymSim: # monte carlo sim for openai gym
     def __init__(self, environment, policy, episodes=10):
         self._environment = environment
         self._policy = policy
@@ -33,4 +56,4 @@ class GymSim:
 
     def run_sim(self, base_return=0.0):
         avg_return = (base_return + self._simulate_eps()) / self._episodes
-        return avg_return.numpy()[0]
+        return avg_return.numpy()[0] #tf dqn agent method
