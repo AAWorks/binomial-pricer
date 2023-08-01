849f6803a47feb6aca1e0fc0a5c930de4ae8d6b5
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-31 20:36:53 -0400

    single step refactoring and research

--- openai_env.py
+++ openai_env.py
@@ -1,17 +1,41 @@
 import gym
 import numpy as np
+import datetime
 
 class Env:
-    def __init__(self, spot=100.0, strike=100.0, r=0.02, sigma=0.20, time=1.0, days=365): 
+    def __init__(self, spot=100.0, strike=100.0, r=0.02, sigma=0.20, today=datetime.date.today(), maturity=None): 
         self._spot = spot
         self._strike = strike
         self._r = r
         self._sigma = sigma
-        self._time = time
-        self._days = days
+
+        if not maturity:
+            maturity = datetime.date(today.year + 1, today.month, today.day)
+
+        delta = maturity - today
+        self._days = delta / datetime.timedelta(days=1)
+        self._float_time = self._days / 365
 
         self._sinit, self._reward, self._day_step = 0, 0, 0
         # from day 0 taking N steps to day N
 
         self.action_space = gym.spaces.Discrete(2) # Two potential classes --> 0: hold, 1:exercise
         self.observation_space = gym.spaces.Box(low=np.array([0, 0]), high=np.array([np.inf, 1.0]), dtype=np.float32)
+    
+    def single_step(self, action: int):
+        if action == 1: # exercise opt
+            reward = max(self._strike - self._spot, 0.0) * np.exp(-self._r * self._float_time * (self._day_step / self._days))
+            done = True
+        else: # hold opt
+            if self._day_step == self._days: # if at maturity
+                reward = max(self._strike - self._spot, 0.0) * np.exp(-self._r * self._time)
+                done = True
+            else: # check tmr
+                reward = 0
+                self._spot = self._spot * np.exp((self._rate - 0.5 * self._sigma ** 2) * (self._time / self._days) + self._sigma * np.sqrt(self._time / self._days) * np.random.normal())
+                self._day_step += 1
+                done = False
+
+        tao = 1.0 - self._day_step / self.days # time remaining in yrs
+
+        return np.array([self.S1, tao]), reward, done, {}
