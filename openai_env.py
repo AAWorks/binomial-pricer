3216ddde9e90369402db77cff8699e0b2603e523
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-07-31 20:39:39 -0400

    env reset

--- openai_env.py
+++ openai_env.py
@@ -16,7 +16,7 @@ class Env:
         self._days = delta / datetime.timedelta(days=1)
         self._float_time = self._days / 365
 
-        self._sinit, self._reward, self._day_step = 0, 0, 0
+        self._s_new, self._reward, self._day_step = 0, 0, 0
         # from day 0 taking N steps to day N
 
         self.action_space = gym.spaces.Discrete(2) # Two potential classes --> 0: hold, 1:exercise
@@ -24,18 +24,24 @@ class Env:
     
     def single_step(self, action: int):
         if action == 1: # exercise opt
-            reward = max(self._strike - self._spot, 0.0) * np.exp(-self._r * self._float_time * (self._day_step / self._days))
+            reward = max(self._strike - self._s_new, 0.0) * np.exp(-self._r * self._float_time * (self._day_step / self._days))
             done = True
         else: # hold opt
             if self._day_step == self._days: # if at maturity
-                reward = max(self._strike - self._spot, 0.0) * np.exp(-self._r * self._time)
+                reward = max(self._strike - self._s_new, 0.0) * np.exp(-self._r * self._time)
                 done = True
             else: # check tmr
                 reward = 0
-                self._spot = self._spot * np.exp((self._rate - 0.5 * self._sigma ** 2) * (self._time / self._days) + self._sigma * np.sqrt(self._time / self._days) * np.random.normal())
+                self._s_new = self._s_new * np.exp((self._rate - 0.5 * self._sigma ** 2) * (self._time / self._days) + self._sigma * np.sqrt(self._time / self._days) * np.random.normal())
                 self._day_step += 1
                 done = False
 
         tao = 1.0 - self._day_step / self.days # time remaining in yrs
 
         return np.array([self.S1, tao]), reward, done, {}
+
+    def reset(self):
+        self._day_step = 0
+        self._s_new = self._spot
+        tao = 1.0 - self._day_step / self._days        # time to maturity, in unit of years
+        return [self.S1, tao]
