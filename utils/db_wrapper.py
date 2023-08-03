3ec2cfcf2c07a2efdb653d62a7e1efc44e3fac63
RomanLearns <romanchenoweth1@gmail.com>
2023-08-02 21:20:37 -0400

    adding rows finished in database

--- utils/db_wrapper.py
+++ utils/db_wrapper.py
@@ -2,6 +2,7 @@ from notanorm import SqliteDb
 from tickers import read_tickers
 import yfinance as yf
 import pandas as pd
+from datetime import datetime
 # from notanorm import MysqlDb 
 
 # accepts all the same parameters as sqlite3.connect
@@ -22,8 +23,8 @@ def current_prices() -> dict:
 def add_row(underlying_ticker, ticker, contract_type, expiration_date, strike_price, price_dict=None):
     if price_dict is None:
         raise Exception("No Price Dict")
-
-    db.insert("options", underlying_ticker=underlying_ticker, ticker=ticker, contract_type=contract_type, expiration_date=expiration_date, srtike_price=strike_price, spot_price=price_dict[underlying_ticker])
+    date = datetime.strptime(expiration_date, '%Y-%m-%d')
+    db.insert("options", underlying_ticker=underlying_ticker, ticker=ticker, contract_type=contract_type, expiration_date=date, srtike_price=strike_price, spot_price=price_dict[underlying_ticker])
     
     
 def add_rows(dataframe : pd.DataFrame):
