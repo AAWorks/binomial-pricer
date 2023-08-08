6d081cb75eb1367cbd158ee35d3456e660c6f1f3
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-08-07 23:16:12 -0400

    updated black scholes to function better with auto diff

--- models/black_scholes.py
+++ models/black_scholes.py
@@ -1,86 +1,91 @@
 import torch
 from torch.distributions import Normal
 
+from datetime import date, timedelta
+
 class BlackScholesOption:
     def __init__(self, 
-                 right="C",
-                 K=torch.tensor(100.0, requires_grad=True), 
-                 S=torch.tensor(100.0, requires_grad=True), 
-                 T = torch.tensor(1.0, requires_grad=True), #time in years
-                 sigma = torch.tensor(0.20, requires_grad=True), 
-                 r = torch.tensor(0.00, requires_grad=True)):
-        self._std_norm_cdf = Normal(0, 1).cdf
-        self._std_norm_pdf = lambda x: torch.exp(Normal(0, 1).log_prob(x))
-        self._option_type = right
-        # Present Value of Exercise Price
-        self._k = K
-        # Current Stock Price
-        self._s = S
-        # Time
-        self._t = T
-        # Volatility
-        self._sigma = sigma
-        # Risk-Free rate
-        self._r = r
+                 option_type: str, 
+                 spot: float, 
+                 strike: float, 
+                 maturity: date, 
+                 implied_volatility: float,
+                 risk_free_rate: float):
+
+        self._option_type = option_type
+
+        self._spot = torch.Tensor(spot)
+        self._spot.requires_grad(True)
+
+        self._strike = torch.Tensor(strike)
+        self._strike.requires_grad(True)
+
+        self._time = torch.Tensor((maturity - date.today()) / timedelta(days=365))
+        self._time.requires_grad(True)
+
+        self._iv = torch.Tensor(implied_volatility)
+        self._iv.requires_grad(True)
+
+        self._r = torch.Tensor(risk_free_rate)
+        self._r.requires_grad(True)
+
+        self._cdf = Normal(0,1).cdf
+        self._pdf = lambda x: Normal(0,1).log_prob(x).exp()
 
     @property
-    def option_type(self): return self._option_type
+    def option_type(self): 
+        return self._option_type
 
     @property
-    def strike_price(self): return self._k
+    def strike_price(self): 
+        return self._strike
 
     @property
-    def spot_price(self): return self._s
+    def spot_price(self): 
+        return self._spot
 
     @property
-    def time(self): return self._t
+    def time(self): 
+        return self._time
 
     @property
-    def volatility(self): return self._sigma
-
-    @property
-    def risk_free_rate(self): return self._r
+    def implied_volatility(self): 
+        return self._iv
     
+    @property
+    def risk_free_rate(self):
+        return self._r
+
+    @property
     def price(self):
-        d_1 = (1 / (self._sigma * torch.sqrt(self._t))) * (torch.log(self._s / self._k) + (self._r + (torch.square(self._s) / 2)) * self._t)
-        d_2 = d_1 - self._sigma * torch.sqrt(self._t)
+        d_1 = (1 / (self._iv * torch.sqrt(self._time))) * (torch.log(self._spot / self._strike) + (self._r + (torch.square(self._spot) / 2)) * self._time)
+        d_2 = d_1 - self._iv * torch.sqrt(self._time)
         
         if self._option_type == "C":
-            C = self._std_norm_cdf(d_1) * self._s - self._std_norm_cdf(d_2) * self._k * torch.exp(-self._r * self._t)
+            C = self._cdf(d_1) * self._spot - self._cdf(d_2) * self._strike * torch.exp(-self._r * self._time)
             return C
             
         elif self._option_type == "P":
-            P = self._std_norm_cdf(-d_2) * self._k * torch.exp(-self._r * self._t) - self._std_norm_cdf(-d_1) * self._s
+            P = self._cdf(-d_2) * self._strike * torch.exp(-self._r * self._time) - self._cdf(-d_1) * self._spot
             return P
-    
-    def compute_greeks(self, price: torch.tensor):
-        price.backward()
+
+    @property
+    def standard_greeks(self):
+        self.price.backward()
+        spot = self._spot
+
         return {
-            "Delta": self._s.grad,
-            "Vega": self._sigma.grad,
-            "Theta": self._t.grad,
-            "Rho": self._r.grad
+            "delta": spot.grad,
+            "rho" : self._r.grad,
+            "vega" : self._iv.grad,
+            "theta" : self._time.grad,
+            "strike_greek" : self._strike.grad
         }
     
-    def exercise_probabilities(self):
-        # probability that option will be exercised
-        d_1 = (1 / (self._sigma * torch.sqrt(self._t))) * (torch.log(self._s / self._k) + (self._r + (torch.square(self._sigma) / 2)) * self._t)
-        # probability that option will be exercised
-        d_2 = d_1 - self._sigma * torch.sqrt(self._t)
-
-    def compute_gamma(self):
-        delta = self._std_norm_cdf(self._d_1)
-        vega = self._s * self._std_norm_pdf(self._d_1) * torch.sqrt(self._t)
-        theta = ((self._s * self._std_norm_pdf(self._d_1) * self._sigma) / (2 * torch.sqrt(self._t))) + self._r * self._k * torch.exp(-self._r * self._t) * self._std_norm_cdf(self._d_2)
-        rho = self._k * self._t * torch.exp(-self._r * self._t) * self._std_norm_cdf(self._d_2)
-
-        S = torch.tensor(100.0, requires_grad=True)
-        price = self._bs_price(self._right, self._k, self._s, self._t, self._sigma, self._r)
-
-        delta = torch.autograd.grad(price, self._s, create_graph=True)[0]
+    @property
+    def gamma(self):
+        spot = self._spot
+        delta = torch.autograd.grad(self.price, spot, create_graph=True)[0]
         delta.backward()
 
-        # And the direct Black-Scholes calculation
-        gamma = self._std_norm_pdf(self._d_1) / (self._s * self._sigma * torch.sqrt(self._t))
-
-        return gamma, self._s.grad
+        return spot.grad
