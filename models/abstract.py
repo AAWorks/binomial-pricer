f4ef1ac56aafb90e748a355e4990997165b06447
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-08-25 15:34:25 -0400

    split prior base abc class into base option and model abc classes

--- models/abstract.py
+++ models/abstract.py
@@ -5,28 +5,24 @@ from abc import ABC, abstractmethod
 from datetime import date, timedelta
 from torch import tensor
 
-
-class Option(ABC):
-    def __init__(self, params, with_tensors=False, name="BaseOption"):
-        self._name = name
-
+class BaseOption(ABC):
+    def __init__(self, params, with_tensors=False):
         try:
             self._maturity = params["maturity"]
-            params["maturity"] = (self._maturity - date.today()) / timedelta(days=365)
+            params["time"] = (self._maturity - date.today()) / timedelta(days=365)
             self._option_type = params["option_type"]
-
-            if with_tensors:
-                params = {k: tensor(v, requires_grad=True) for k, v in params.items() if k != "option_type"}
 
             self._spot = params["spot"]
             self._strike = params["strike"]
-            self._time = params["maturity"]
+            self._time = tensor(params["time"], requires_grad=True) if with_tensors else params["time"]
             self._iv = params["implied_volatility"]
             self._r = params["risk_free_rate"]
             self._d = params["dividend_rate"]
         except Exception as e:
-            st.error(f"Missing Params - {e}")
-            raise Exception("Missing Params")
+            msg = f"<Option> Missing Params - {e} - All Params: {params}"
+            custom_msg = None
+            st.error(msg if not custom_msg else custom_msg)
+            raise Exception(msg)
 
     @property
     def option_type(self):
@@ -55,16 +51,33 @@ class Option(ABC):
     @property
     def dividend_rate(self):
         return self._d
+    
+    @property
+    def maturity(self):
+        return self._maturity
 
+
+class Model(BaseOption):
+    def __init__(self, params, with_tensors=False, name="BaseOption"):
+        self._name = name
+
+        try:
+            if with_tensors:
+                params = {k: (tensor(v, requires_grad=True) 
+                              if k not in ("option_type", "maturity") else v) 
+                              for k, v in params.items()}
+
+            super().__init__(params, with_tensors=with_tensors)
+
+        except Exception as e:
+            msg = f"<Priced Option> Tensor/Base - {e}"
+            st.error(msg)
+            raise Exception(msg)
+    
     @property
     @abstractmethod
     def npv(self):
         return -1
-
-    @property
-    @abstractmethod
-    def greeks(self):
-        pass
 
     def __str__(self):
         return f"Option Price ({self._name} Model): ${self.npv}"
